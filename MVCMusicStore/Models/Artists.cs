d3106654c14dd6c74b493898eb01b84749013963
jjlane4 <jml7128@uncw.edu>
2016-02-16 21:01:35 -0500

    originalCommit

---
+++ MVCMusicStore/Models/Artists.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+    public class Artist
+    {
+
+        public int ArtistID { get; set; }
+
+        public string Name { get; set; }
+
+        public int AlbumID { get; set; }
+
+        public virtual List<Album> Album { get; set; }
+    }
+}
